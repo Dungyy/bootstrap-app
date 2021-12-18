e38fbd5681f8e987f5927cfc329351995488ce50
Dungyy <erickmunoz13@gmail.com>
2021-12-18 02:36:54 -0600

    started bootstrap project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+   return (
+    <React.Fragment> 
+      <Router>
+        <Switch>
+          <Route exact path="/" component />
+          <Route exact path="/about" component />
+          <Route exact path="/contact" component />
+          <Route component />
+        </Switch>
+      </Router>
+    </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/componets/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Home = () => (
+        <div>
+            <h1>Welcome Home</h1>
+        </div>
+    )
+
